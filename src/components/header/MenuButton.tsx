b3b8cc38c07952410cb3c01d03bb83729b9a03c1
mo3ito <mostafa.tar90@gmail.com>
2024-10-20 18:42:25 +0330

    add MobileModeHead

---
+++ src/components/header/MenuButton.tsx
@@ -0,0 +1,22 @@
+import useTheme from "../../hooks/useThemeContext";
+
+export default function MenuButton() {
+  const { theme } = useTheme();
+  return (
+    <button>
+      <svg
+        className={`${
+          theme === "dark" ? "fill-[#EAECEF]" : "fill-[#1E2329]"
+        } size-6`}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4 4h16v3H4V4zm0 6.5h16v3H4v-3zM20 17H4v3h16v-3z"
+        ></path>
+      </svg>
+    </button>
+  );
+}
--- src/components/ui/header/MobileModeHeader.tsx
+++ src/components/ui/header/MobileModeHeader.tsx
@@ -1,11 +1,12 @@
 import LogoSvg from "../../svgs/LogoSvg";
 import Button from "../../header/Button";
 import useTheme from "../../../hooks/useThemeContext";
+import MenuButton from "../../header/MenuButton";
 
 export default function MobileModeHeader() {
   const { theme } = useTheme();
   return (
-    <header className="  flex md:hidden items-center justify-between pr-4">
+    <header className="flex md:hidden items-center justify-between pr-4">
       <LogoSvg />
       <section className="flex items-center gap-x-4">
         <Button
@@ -16,19 +17,7 @@ export default function MobileModeHeader() {
               : "bg-[#FCD535] text-[#202630] "
           } hover:opacity-80`}
         />
-        <svg
-          className={`${
-            theme === "dark" ? "fill-[#EAECEF]" : "fill-[#1E2329]"
-          } size-6`}
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M4 4h16v3H4V4zm0 6.5h16v3H4v-3zM20 17H4v3h16v-3z"
-          ></path>
-        </svg>
+        <MenuButton />
       </section>
     </header>
   );
