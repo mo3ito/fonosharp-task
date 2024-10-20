5f93bafcf541f21d6fdc191aff6928f84fe03834
mo3ito <mostafa.tar90@gmail.com>
2024-10-20 13:17:43 +0330

    add layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/ui/Layout.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ path: "/", element: <App /> }],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router}/>
+);
